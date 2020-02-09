f7460107bcaf57cc76f171fd45cf9c5e8a4c4384
Kuroyza <kuroyza@gmail.com>
2020-02-09 14:41:09 +0100

    fixed click several cards bug

--- src/views/Projects/CardMatching.vue
+++ src/views/Projects/CardMatching.vue
@@ -24,20 +24,22 @@
          </li>
 
          <div class="card-bar">
-            <div class="card-bar--inner"  :style="{height: 100 - (failed / max) * 100 + '%'}"></div>
-
+            <div class="card-bar--inner"  :style="{height: (lifes / max) * 100 + '%'}">{{lifes}}/{{max}}</div>
          </div>
       </ul>
-
-      <!-- TODO: Make this work
-         <div class="notify">
-         <button class="notify__btn">Play again</button>
-      </div> -->
+      
+      <div class="notify" v-if="showNotify">
+         <h2 class="notify--headline">{{msg}}</h2>
+         <button class="notify--btn" @click="resetAll">Play again</button>
+      </div>
    </div>
 </template>
 
 
 <script>
+
+// TODO: Add Game Levels
+
 export default {
    data: function(){
       return{
@@ -66,14 +68,28 @@ export default {
          choices: [],
          counter: 0,
          max: 7,
-         failed: 0
+         lifes: 7,
+         wait: false,
+         msg: '',
+         showNotify: false
       }
    },
    methods: {
       flipCard(card){
-         if(card.classList.contains('done') || this.counter >= 6 || this.failed >= this.max){
-            console.log('No No No!!!!');
+         if(card.classList.contains('done')){
             return;
+         }
+
+         if(this.counter >= 6 || this.lifes <= 0){
+            this.showMsg();
+            return;
+         }
+
+         if(this.wait){
+            this.wait = false;
+            this.choices[0].classList.remove('active');
+            this.choices[1].classList.remove('active');
+            this.choices.splice(0);
          }
 
          if(this.choices.length >= 2){
@@ -94,10 +110,23 @@ export default {
          }
       },
       checkWinningCard(){
-         if(this.choices[0].innerText == this.choices[1].innerText){
-            this.winningCards();
+         if(this.choices[0].innerText != this.choices[1].innerText){
+            this.wait = true;
+            this.lifes--;
+
+            setTimeout(() => {
+
+               if(this.choices.length > 0 && this.wait){
+                  this.wait = false;
+                  this.choices[0].classList.remove('active');
+                  this.choices[1].classList.remove('active');
+                  this.choices.splice(0);
+               }
+            }, 1150);
+            return;
          }
-         
+
+         this.winningCards();
       },
       winningCards(){
          console.log('before: ', this.choices);
@@ -111,12 +140,33 @@ export default {
          this.counter++;
 
          if(this.counter >= 6){  
-            alert("You won");
+            this.showMsg();
          }
 
-
          console.log('after: ', this.choices);
+      },
+      resetAll(){
+         this.max = 7;
+         this.lifes = this.max;
+         this.showNotify = false;
+         this.msg = '';
+      },
+      showMsg(){
+         this.showNotify = true;
+         if(this.lifes <= 0){
+            this.msg = 'You LOSE!';
+         }else if(this.counter >= this.emojis.length / 2){
+            this.msg = 'You WON!';
+         }
+      },
+      randomPositions(){
+         // TODO: Get random from the existing array
+         // this.emojis = [];
+         console.log('I am Random Positions');
       }
+   },
+   beforeMount(){
+      this.randomPositions();
    }
 }
 
@@ -189,7 +239,7 @@ export default {
       top: 0;
       transform: translateX(300%);
       height: 100%;
-      width: 25px;
+      width: 50px;
       box-shadow: inset 0 0 5px rgba(0,0,0,.5);
       border-bottom-left-radius: 9px;
       border-bottom-right-radius: 9px;
@@ -202,7 +252,39 @@ export default {
          right: 0;
          width: 100%;
          background-color: orangered;
+         display: flex;
+         justify-content: center;
+         align-items: center;
+         color: #fff;
          
+      }
+   }
+
+   .notify{
+      position: fixed;
+      right: 0;
+      top: 0;
+      background-color: rgba(0,0,0,.35);
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+      align-items: center;
+
+      &--headline{
+         font-size: 5rem;
+         color: orangered;
+         text-shadow: 0 0 5px rgba(255,255,255,.5);
+      }
+
+      &.success &--headline{
+         color: yellowgreen;
+      }
+
+      &--btn{
+         font-size: 2rem;
+         padding: 1rem 1.5rem;
       }
    }
    
